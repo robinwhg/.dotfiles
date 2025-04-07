5a4ae56acc651d00a044e3e232b3ecd338330c8e
robinwhg <mail@robinwhg.com>
2025-04-07 09:38:41 +0200

    disable volar formatting if eslint is attached

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -18,55 +18,25 @@ return {
         prefix = "",
       },
     },
+    inlay_hints = {
+      enabled = false,
+    },
+    servers = {
+      eslint = {},
+      volar = {},
+    },
+    setup = {
+      eslint = function()
+        require("lazyvim.util").lsp.on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "vtsls" then
+            client.server_capabilities.documentFormattingProvider = false
+          elseif client.name == "volar" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
+      end,
+    },
   },
-  -- servers = { eslint = {} },
-  -- setup = {
-  --   eslint = function()
-  --     require("lazyvim.util").lsp.on_attach(function(client)
-  --       if client.name == "eslint" then
-  --         client.server_capabilities.documentFormattingProvider = true
-  --       elseif client.name == "vue-language-server" then
-  --         client.server_capabilities.documentFormattingProvider = false
-  --       elseif client.name == "vtsls" then
-  --         client.server_capabilities.documentFormattingProvider = false
-  --       end
-  --     end)
-  --   end,
-  -- },
 }
--- TODO: Enable prettier if not eslint, disable ts if volar
-
--- return {
---   "neovim/nvim-lspconfig",
---   opts = {
---     diagnostics = {
---       float = {
---         style = "minimal",
---         border = "rounded",
---         source = "always",
---         header = "",
---         prefix = "",
---       },
---     },
---     inlay_hints = {
---       enabled = false,
---     },
---     servers = {
---       eslint = {},
---       volar = {},
---     },
---     setup = {
---       eslint = function()
---         require("lazyvim.util").lsp.on_attach(function(client)
---           if client.name == "eslint" then
---             client.server_capabilities.documentFormattingProvider = true
---           elseif client.name == "vtsls" then
---             client.server_capabilities.documentFormattingProvider = false
---           elseif client.name == "volar" then
---             client.server_capabilities.documentFormattingProvider = false
---           end
---         end)
---       end,
---     },
---   },
--- }
