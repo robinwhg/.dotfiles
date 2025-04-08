cc696ba1fbe1e8cc0e6a46922343a45c5bf8e878
robinwhg <mail@robinwhg.com>
2025-04-08 18:18:13 +0200

    config

--- nvim/.config/nvim/lua/plugins/copilotchat.lua
+++ nvim/.config/nvim/lua/plugins/copilotchat.lua
@@ -1,10 +1,32 @@
 return {
   "CopilotC-Nvim/CopilotChat.nvim",
+  branch = "main",
+  cmd = "CopilotChat",
   opts = function()
+    local user = vim.env.USER or "User"
     return {
+      auto_insert_mode = true,
+      question_header = "  " .. user .. " ",
+      answer_header = "  Copilot ",
+      model = "claude-3.7-sonnet",
       window = {
-        width = 0.20,
+        width = 40,
       },
     }
+  end,
+  config = function(_, opts)
+    local chat = require("CopilotChat")
+
+    vim.api.nvim_create_autocmd("BufEnter", {
+      pattern = "copilot-chat",
+      callback = function()
+        vim.opt_local.relativenumber = false
+        vim.opt_local.number = false
+        vim.opt_local.signcolumn = "no"
+        vim.opt_local.winfixwidth = true
+      end,
+    })
+
+    chat.setup(opts)
   end,
 }
--- nvim/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/plugins/neo-tree.lua
@@ -2,7 +2,7 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     window = {
-      width = 32,
+      width = 40,
     },
     filesystem = {
       filtered_items = {
