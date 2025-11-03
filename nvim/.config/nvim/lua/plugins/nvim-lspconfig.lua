c2094591a46aa8714430999494f93904448bcfe6
robinwhg <mail@robinwhg.com>
2025-11-03 15:45:06 +0100

    update

--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,42 +1,22 @@
-local getKeys = function()
-  local keys = require("lazyvim.plugins.lsp.keymaps").get()
-  keys[#keys + 1] = { "<c-k>", false, mode = "i" }
-
-  return keys
-end
-
 return {
   "neovim/nvim-lspconfig",
-  opts = {
-    keys = getKeys(),
-    diagnostics = {
-      float = {
-        style = "minimal",
-        border = "rounded",
-        source = "always",
-        header = "",
-        prefix = "",
+  opts = function(_, opts)
+    table.insert(opts.servers.vtsls.filetypes, "vue")
+    LazyVim.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
+      {
+        name = "@vue/typescript-plugin",
+        location = LazyVim.get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
+        languages = { "vue" },
+        configNamespace = "typescript",
+        enableForWorkspaceTypeScriptVersions = true,
       },
-    },
-    inlay_hints = {
-      enabled = false,
-    },
-    servers = {
-      eslint = {},
-      volar = {},
-    },
-    setup = {
-      eslint = function()
-        require("lazyvim.util").lsp.on_attach(function(client)
-          if client.name == "eslint" then
-            client.server_capabilities.documentFormattingProvider = true
-          elseif client.name == "vtsls" then
-            client.server_capabilities.documentFormattingProvider = false
-          elseif client.name == "volar" then
-            client.server_capabilities.documentFormattingProvider = false
-          end
-        end)
-      end,
-    },
-  },
+    })
+    opts.diagnostics.float = {
+      style = "minimal",
+      border = "rounded",
+      source = "always",
+      header = "",
+      prefix = "",
+    }
+  end,
 }
