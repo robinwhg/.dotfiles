c6610499bf7e391de627c24073c6e21ea6592635
robinwhg <mail@robinwhg.com>
2026-02-06 11:51:51 +0100

    feat(fish): add pnpm configuration

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -23,3 +23,10 @@ end
 
 # opencode
 fish_add_path /home/robinwhg/.opencode/bin
+
+# pnpm
+set -gx PNPM_HOME "/home/robinwhg/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
